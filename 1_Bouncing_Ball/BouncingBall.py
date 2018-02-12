d3c6f46a42db3bcc3375b5e3cdab27e82e7efc23
Obsidienne <obsidienne@protonmail.com>
2018-02-12 23:23:05 +0100

    BouncingBall - Ex1

--- 1_Bouncing_Ball/BouncingBall.py
+++ 1_Bouncing_Ball/BouncingBall.py
@@ -7,27 +7,28 @@ GREEN = [0, 255, 0]
 RED   = [255, 0, 0]
 BLUE  = [0 , 0 , 255]
 
-# initialisation de l'écran de jeu
+# Initialization
 pygame.init()
 
-# Set the height and width of the screen
-screen_size = [400, 300]
+screen_size = [800, 600]
 screen = pygame.display.set_mode(screen_size)
 
-pygame.display.set_caption("Bouncing Rectangle")
+pygame.display.set_caption("Bouncing Ball !!!")
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
 # Starting position
 box_x = 50
-box_y = 50
+box_y = screen_size[1] // 2
 
 # Speed and direction
 box_change_x = 3
 box_change_y = 3
 
-box_rayon= 5
+box_rayon = 10
+
+box_state = 0
 
 # Loop until the user clicks the close button.
 done = False
@@ -50,19 +51,21 @@ while not done:
     # Rebond
     if box_y > screen_size[1] or box_y < 0:
         box_change_y = box_change_y * -1
+        box_state = 1 - box_state
     if box_x > screen_size[0] or box_x < 0:
         box_change_x = box_change_x * -1
+        box_state = 1 - box_state
 
     #DESSIN
     # Set the screen background
-    screen.fill(BLACK)
+    screen.fill(WHITE)
 
     # Draw screen border
-    pygame.draw.rect(screen,GREEN,[0,0, screen_size[0] , screen_size[1]],1)
+    pygame.draw.rect(screen,GREEN,[0, 0, screen_size[0], screen_size[1]], 5)
 
     #dessine le palet
-    pygame.draw.circle(screen, WHITE, [box_x, box_y], box_rayon *2)
-    pygame.draw.circle(screen, RED, [box_x, box_y], box_rayon )
+    pygame.draw.circle(screen, BLUE, [box_x, box_y], box_rayon *2)
+    pygame.draw.circle(screen, RED if box_state == 0 else GREEN, [box_x, box_y], box_rayon )
 
     # Limit to 30 frames per second
     clock.tick(30)
