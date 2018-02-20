2bd95a2323288d84a811200122030c0c796d08e3
Obsidienne <obsidienne@protonmail.com>
2018-02-20 23:22:02 +0100

    lemmings - Walk (left)

--- lemmings/lemming.py
+++ lemmings/lemming.py
@@ -2,23 +2,34 @@ import enum
 import types
 import pygame
 
+from shared.collisions import pixel_collision_mid
 from shared.animated_sprite import AnimatedSprite, Animations
 from lemmings.common import asset_path
 
-STATES = enum.Enum("STATES", "WALK FALL")
+STATES = enum.Enum("STATES", "START WALK FALL")
 
 class Fall:
     def __init__(self, lemming):
         self.lemming = lemming
-        self.start()
 
     def start(self):
-        self.lemming.sprite.animations.set("FALL")
+        self.lemming.sprite.animations.start("FALL")
         self.fallcount = 0
 
     def run(self):
         self.lemming.sprite.rect.move_ip(0, 3)
         self.fallcount += 3
+
+class Walk:
+    def __init__(self, lemming):
+        self.lemming = lemming
+
+    def start(self):
+        self.dx = -1
+
+    def run(self):
+        if self.dx < 0: self.lemming.sprite.animations.set("WALK_L")
+        self.lemming.sprite.rect.move_ip(self.dx, 0)
 
 class Lemming:
     lemming_imgs = AnimatedSprite.spritesheet_to_images(
@@ -37,7 +48,7 @@ class Lemming:
                     "WALK_L": range(0, 8),
                     "FALL"  : range(8, 12),
                 },
-                default = "FALL",
+                default = "WALK_L",
                 period  = 100,
             ),
             position = (250, 100)
@@ -45,15 +56,40 @@ class Lemming:
         self.sprite.colorkey(pygame.Color("black"))
 
         self.behaviours = types.SimpleNamespace(
+            walk = Walk(self),
             fall = Fall(self),
         )
 
-        self.state = STATES.FALL
+        self.state = STATES.START
 
     def update(self):
         self.sprite.update()
 
+        if self.state == STATES.START:
+            self.behaviours.walk.start()
+            self.state = STATES.WALK
+
+        if self.state == STATES.WALK:
+            fall = pixel_collision_mid(
+                self.window.screen, self.sprite.rect, pygame.Color("black")
+            ).down
+
+            if fall:
+                self.state = STATES.FALL
+                self.behaviours.fall.start()
+                return
+
+            self.behaviours.walk.run()
+
         if self.state == STATES.FALL:
+            walk = not pixel_collision_mid(
+                self.window.screen, self.sprite.rect, pygame.Color("black")
+            ).down
+            
+            if walk:
+                self.state = STATES.WALK
+                return
+
             self.behaviours.fall.run()
 
     def draw(self): self.sprite.draw(self.window.screen)
