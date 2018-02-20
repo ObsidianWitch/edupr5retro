2e5bd2e5a6e73685efcde4593d673d66770ec5fc
Obsidienne <obsidienne@protonmail.com>
2018-02-21 00:50:02 +0100

    lemmings - Dead

--- lemmings/lemming.py
+++ lemmings/lemming.py
@@ -3,22 +3,23 @@ import types
 import pygame
 
 from shared.collisions import pixel_collision_mid
-from shared.animated_sprite import AnimatedSprite, Animations
+from shared.animated_sprite import AnimatedSprite, Animations, Animation
 from lemmings.common import asset_path
 
-STATES = enum.Enum("STATES", "START WALK FALL")
+STATES = enum.Enum("STATES", "START WALK FALL DEAD")
 
 class Fall:
     def __init__(self, lemming):
         self.lemming = lemming
 
     def start(self):
-        self.lemming.sprite.animations.start("FALL")
+        self.lemming.animations.start("FALL")
         self.fallcount = 0
 
     def run(self):
-        self.lemming.sprite.rect.move_ip(0, 3)
+        self.lemming.rect.move_ip(0, 3)
         self.fallcount += 3
+        return (self.fallcount >= 100)
 
 class Walk:
     def __init__(self, lemming):
@@ -28,10 +29,20 @@ class Walk:
         self.dx = -1
 
     def run(self):
-        if self.dx < 0: self.lemming.sprite.animations.set("WALK_L")
-        self.lemming.sprite.rect.move_ip(self.dx, 0)
+        if self.dx < 0: self.lemming.animations.set("WALK_L")
+        self.lemming.rect.move_ip(self.dx, 0)
 
-class Lemming:
+class Dead:
+    def __init__(self, lemming):
+        self.lemming = lemming
+
+    def start(self):
+        self.lemming.animations.start("DEAD")
+
+    def run(self):
+        if self.lemming.animations.finished: self.lemming.kill()
+
+class Lemming(AnimatedSprite):
     lemming_imgs = AnimatedSprite.spritesheet_to_images(
         path          = asset_path("planche.png"),
         sprite_size   = (30, 30),
@@ -41,55 +52,59 @@ class Lemming:
     def __init__(self, window):
         self.window = window
 
-        self.sprite = AnimatedSprite(
-            images = self.lemming_imgs,
+        AnimatedSprite.__init__(
+            self       = self,
+            images     = self.lemming_imgs,
             animations = Animations(
                 data = {
-                    "WALK_L": range(0, 8),
-                    "FALL"  : range(8, 12),
+                    "WALK_L": Animation(range(0, 8)),
+                    "FALL"  : Animation(range(8, 12)),
+                    "DEAD"  : Animation(range(117, 133), loop = False),
                 },
-                default = "WALK_L",
                 period  = 100,
             ),
             position = (250, 100)
         )
-        self.sprite.colorkey(pygame.Color("black"))
+        self.colorkey(pygame.Color("black"))
 
         self.behaviours = types.SimpleNamespace(
             walk = Walk(self),
             fall = Fall(self),
+            dead = Dead(self),
         )
 
         self.state = STATES.START
 
     def update(self):
-        self.sprite.update()
+        AnimatedSprite.update(self)
 
         if self.state == STATES.START:
             self.behaviours.walk.start()
             self.state = STATES.WALK
 
         if self.state == STATES.WALK:
+            self.behaviours.walk.run()
+
             fall = pixel_collision_mid(
-                self.window.screen, self.sprite.rect, pygame.Color("black")
+                self.window.screen, self.rect, pygame.Color("black")
             ).down
 
             if fall:
                 self.state = STATES.FALL
                 self.behaviours.fall.start()
-                return
-
-            self.behaviours.walk.run()
 
         if self.state == STATES.FALL:
+            dead = self.behaviours.fall.run()
+
             walk = not pixel_collision_mid(
-                self.window.screen, self.sprite.rect, pygame.Color("black")
+                self.window.screen, self.rect, pygame.Color("black")
             ).down
-            
-            if walk:
+
+            if dead and walk:
+                self.state = STATES.DEAD
+                self.behaviours.dead.start()
+            elif not dead and walk:
                 self.state = STATES.WALK
-                return
 
-            self.behaviours.fall.run()
-
-    def draw(self): self.sprite.draw(self.window.screen)
+        if self.state == STATES.DEAD:
+            self.behaviours.dead.run()
--- lemmings/lemmings.py
+++ lemmings/lemmings.py
@@ -1,22 +1,23 @@
-from shared.timer import Timer
+import pygame
 
-from lemmings.lemming  import Lemming
+from shared.timer import Timer
+from lemmings.lemming import Lemming
 
 class Lemmings:
     def __init__(self, window):
         self.window = window
 
-        self.lst = []
+        self.group = pygame.sprite.Group()
+        self.counter = 0
         self.max = 15
         self.timer = Timer(end = 15, period = 100)
 
     def generate(self):
-        if (len(self.lst) < self.max) and self.timer.finished:
-            self.lst.append(Lemming(self.window))
+        if (self.counter < self.max) and self.timer.finished:
+            self.group.add(Lemming(self.window))
+            self.counter += 1
             self.timer.restart()
 
-    def update(self):
-        for lemming in self.lst: lemming.update()
+    def update(self): self.group.update()
 
-    def draw(self):
-        for lemming in self.lst: lemming.draw()
+    def draw(self): self.group.draw(self.window.screen)
