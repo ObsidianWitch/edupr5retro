d892a93a23e16059b40ab77531f720215c929ea4
Obsidienne <obsidienne@protonmail.com>
2018-02-20 22:09:03 +0100

    shared - collisions

--- maze/state_run.py
+++ maze/state_run.py
@@ -1,8 +1,7 @@
-import math
-
 import pygame
 
 from shared.math  import Directions
+from shared.collisions import *
 from maze.palette import palette
 from maze.maze    import Maze
 from maze.player  import Player
@@ -13,37 +12,6 @@ class StateRun:
 
         self.player = Player(window)
         self.maze = Maze(window)
-
-    # Check collision between `sprite.rect`'s topleft, topright, bottomleft,
-    # bottomright points and adjacent pixels of the specified `color`.
-    # Only checking collision against `sprite.rect` midpoints would have caused
-    # missed collision detection in some cases (near the rect's vertices).
-    # To avoid this problem, we check the state of adjacent pixels to the vertices
-    # of the current edge (e.g. topleft + (1, 0) and bottomleft + (1, 0) for the
-    # left edge)
-    def pixel_collision(self, rect, color):
-        def check(p, vec):
-            return (self.window.screen.get_at((
-                p[0] + vec[0],
-                p[1] + vec[1]
-            )) == color)
-
-        return Directions(
-            up    = check(rect.topleft,  (0, -1))
-                 or check(rect.topright, (0, -1)),
-            down  = check(rect.bottomleft,  (0, 1))
-                 or check(rect.bottomright, (0, 1)),
-            left  = check(rect.topleft,    (-1, 0))
-                 or check(rect.bottomleft, (-1, 0)),
-            right = check(rect.topright,    (1, 0))
-                 or check(rect.bottomright, (1, 0)),
-    )
-
-    def distance_collision(self, p1, p2, threshold):
-        return math.sqrt(
-                math.pow(p2[0] - p1[0], 2)
-              + math.pow(p2[1] - p1[1], 2)
-        ) < threshold
 
     def draw_score(self):
         score_surface = self.window.fonts[0].render(
@@ -67,9 +35,10 @@ class StateRun:
                 left  = self.window.keys[pygame.K_LEFT],
                 right = self.window.keys[pygame.K_RIGHT],
             ),
-            collisions = self.pixel_collision(
-                rect  = self.player.sprite.rect,
-                color = pygame.Color(*palette["B"])
+            collisions = pixel_collision_vertices(
+                surface = self.window.screen,
+                rect    = self.player.sprite.rect,
+                color   = pygame.Color(*palette["B"]),
             ),
         )
 
@@ -88,7 +57,7 @@ class StateRun:
         ): self.player.score += 100
 
         ## Exit
-        if self.distance_collision(
+        if distance_collision(
             p1 = self.player.sprite.rect.center,
             p2 = self.maze.exit.rect.center,
             threshold = 5
---
+++ shared/collisions.py
@@ -0,0 +1,50 @@
+import math
+from shared.math import Directions
+
+# Check collisions between `rect` midpoints and adjacent pixels of the specified
+# `color` in `surface`.
+def pixel_collision_mid(surface, rect, color):
+    def check(p, offset):
+        return (surface.get_at((
+            p[0] + offset[0],
+            p[1] + offset[1]
+        )) == color)
+
+    return Directions(
+        up    = check(rect.midtop,    ( 0, -1)),
+        down  = check(rect.midbottom, ( 0,  1)),
+        left  = check(rect.midleft,   (-1,  0)),
+        right = check(rect.midright,  ( 1,  0)),
+    )
+
+
+# Check collision between `rect`'s topleft, topright, bottomleft,
+# bottomright points and adjacent pixels of the specified `color` in `surface`.
+# Only checking collision against `rect` midpoints would have caused
+# missed collision detection in some cases (near the rect's vertices).
+# To avoid this problem, we check the state of adjacent pixels to the vertices
+# of the current edge (e.g. topleft + (1, 0) and bottomleft + (1, 0) for the
+# left edge)
+def pixel_collision_vertices(surface, rect, color):
+    def check(p, offset):
+        return (surface.get_at((
+            p[0] + offset[0],
+            p[1] + offset[1]
+        )) == color)
+
+    return Directions(
+        up    = check(rect.topleft,     ( 0, -1))
+             or check(rect.topright,    ( 0, -1)),
+        down  = check(rect.bottomleft,  ( 0,  1))
+             or check(rect.bottomright, ( 0,  1)),
+        left  = check(rect.topleft,     (-1,  0))
+             or check(rect.bottomleft,  (-1,  0)),
+        right = check(rect.topright,    ( 1,  0))
+             or check(rect.bottomright, ( 1,  0)),
+    )
+
+def distance_collision(p1, p2, threshold):
+    return math.sqrt(
+            math.pow(p2[0] - p1[0], 2)
+          + math.pow(p2[1] - p1[1], 2)
+    ) < threshold
