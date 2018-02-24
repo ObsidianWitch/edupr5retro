cb794d7dbe69ea5d093d3574f30267b725492164
Obsidienne <obsidienne@protonmail.com>
2018-02-24 12:18:32 +0100

    shared - fix collisions' offsets + pixel_checker

--- shared/collisions.py
+++ shared/collisions.py
@@ -1,11 +1,7 @@
 import math
 from shared.math import Directions
 
-# Check collisions between `rect` midpoints and adjacent pixels of the specified
-# `color` in `surface`.
-# For each direction, returns True on collision, False on non-collisions, and
-# None in case of undefined behaviour (e.g. outside of the `surface` rect).
-def pixel_collision_mid(surface, rect, color):
+def pixel_checker(surface, color):
     def inside(p): return surface.get_rect().collidepoint(p)
 
     def check(p, offset):
@@ -13,11 +9,20 @@ def pixel_collision_mid(surface, rect, color):
         if not inside(p): return None
         return (surface.get_at(p) == color)
 
+    return check
+
+# Check collisions between `rect` midpoints and adjacent pixels of the specified
+# `color` in `surface`.
+# For each direction, returns True on collision, False on non-collisions, and
+# None in case of undefined behaviour (e.g. outside of the `surface` rect).
+def pixel_collision_mid(surface, rect, color):
+    check = pixel_checker(surface, color)
+
     return Directions(
         up    = check(rect.midtop,    ( 0, -1)),
-        down  = check(rect.midbottom, ( 0,  1)),
+        down  = check(rect.midbottom, ( 0,  0)),
         left  = check(rect.midleft,   (-1,  0)),
-        right = check(rect.midright,  ( 1,  0)),
+        right = check(rect.midright,  ( 0,  0)),
     )
 
 # Check collision between `rect`'s topleft, topright, bottomleft,
@@ -25,22 +30,17 @@ def pixel_collision_mid(surface, rect, color):
 # For each direction, returns True on collision, False on non-collisions, and
 # None in case of undefined behaviour (e.g. outside of the `surface` rect).
 def pixel_collision_vertices(surface, rect, color):
-    def inside(p): return surface.get_rect().collidepoint(p)
-
-    def check(p, offset):
-        p = (p[0] + offset[0], p[1] + offset[1])
-        if not inside(p): return None
-        return (surface.get_at(p) == color)
+    check = pixel_checker(surface, color)
 
     return Directions(
         up    = check(rect.topleft,     ( 0, -1))
-             or check(rect.topright,    ( 0, -1)),
-        down  = check(rect.bottomleft,  ( 0,  1))
-             or check(rect.bottomright, ( 0,  1)),
+             or check(rect.topright,    (-1, -1)),
+        down  = check(rect.bottomleft,  ( 0,  0))
+             or check(rect.bottomright, (-1,  0)),
         left  = check(rect.topleft,     (-1,  0))
-             or check(rect.bottomleft,  (-1,  0)),
-        right = check(rect.topright,    ( 1,  0))
-             or check(rect.bottomright, ( 1,  0)),
+             or check(rect.bottomleft,  (-1, -1)),
+        right = check(rect.topright,    ( 0,  0))
+             or check(rect.bottomright, ( 0, -1)),
     )
 
 def distance_collision(p1, p2, threshold):
