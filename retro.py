e6a75316f19aa4aa744bd79c5d92b6d73867430a
Obsidienne <obsiagae@gmail.com>
2018-03-07 13:36:47 +0100

    working base

--- retro.py
+++ retro.py
@@ -1,12 +1,12 @@
 import tkinter
 import os, inspect
-import time 
+import time
 import PIL
 from PIL import Image
-from PIL import ImageFont 
+from PIL import ImageFont
 from PIL import ImageTk
 from PIL import ImageDraw
- 
+
 ######################################################################
 #
 #   Mise en place de la partie technique | ne pas toucher
@@ -21,25 +21,25 @@ def ColorKey(image,color):
        else:
         newData.append(item)
     return img
-        
 
 
-scriptPATH = os.path.abspath(inspect.getsourcefile(lambda:0)) 
+
+scriptPATH = os.path.abspath(inspect.getsourcefile(lambda:0))
 scriptDIR  = os.path.dirname(scriptPATH)
-assets = os.path.join(scriptDIR,"data")
-myfont = ImageFont.truetype(font='arial.ttf', size=30);
+assets = os.path.join(scriptDIR,"assets")
+myfont = ImageFont.truetype(font='assets/DejaVuSansMono.ttf', size=30)
 
 
 KEYPressed = set()
 OnBoucle = True
 window = tkinter.Tk()
 WIDTH, HEIGHT = 640, 480
-buffer = Image.new('RGBA', (WIDTH,HEIGHT)) 
+buffer = Image.new('RGBA', (WIDTH,HEIGHT))
 draw = ImageDraw.Draw(buffer)
 
 def keyup(e):
     KEYPressed.remove(e.keysym)
-    
+
 def keydown(e):
     KEYPressed.add(e.keysym)
     #print(e.keysym)
@@ -50,12 +50,12 @@ def Fermeture():
 def MouseClick(event):
     window.focus_set()
     print("clicked at", event.x, event.y)
-    
+
 def ReadMouseXY():
    x = window.winfo_pointerx()
    y = window.winfo_pointery()
    ax = window.winfo_pointerx() - window.winfo_rootx()
-   ay = window.winfo_pointery() - window.winfo_rooty()  
+   ay = window.winfo_pointery() - window.winfo_rooty()
    if ( ax < 0) : ax = 0
    if ( ax >= WIDTH ) : ax = WIDTH-1
    if ( ay < 0) : ay = 0
@@ -91,21 +91,21 @@ fff = 0
 while(OnBoucle):
     timestart = time.time()
     (MouseX,MouseY) = ReadMouseXY()
-    
+
     #################################################################
     #
-    #  logique 
-  
-    
+    #  logique
+
+
     if ( 'Escape' in KEYPressed ):  # https://www.tcl.tk/man/tcl8.4/TkCmd/keysyms.htm
         OnBoucle = False
-    
-    if ( 'Left' in KEYPressed ): 
+
+    if ( 'Left' in KEYPressed ):
        fff -= 5
-        
+
     if ( 'Right' in KEYPressed ):
        fff += 5
-    
+
     #################################################################
     #
     #  affichage
@@ -114,29 +114,29 @@ while(OnBoucle):
     zone_jaune = decor.crop((fff,0,fff+WIDTH,HEIGHT)) # selectionne la zone a afficher dans le décors (x1,y1,x2,y2)
     buffer.paste(zone_jaune)
     buffer.alpha_composite(bandit,(100,HEIGHT-bandit.height)) #affichage avec transparence
-    
+
     bandit_small = bandit.resize((bandit.width // 2, bandit.height // 2 ))  # // pour avoir des valeurs entieres
     buffer.alpha_composite(bandit_small,(200,HEIGHT-bandit_small.height))
-    
+
     bandit_small = bandit_small.rotate(180)
     buffer.alpha_composite(bandit_small,(300,HEIGHT-bandit_small.height))
-    
- 
+
+
     C = buffer.getpixel((MouseX,MouseY)) # valeur (R,V,B) ou (R,V,B,A) A = transparence
     print ( 'couleur : ',C[0], C[1],C[2])
-     
- 
+
+
     draw.ellipse(((MouseX-5, MouseY-5), (MouseX+5, MouseY+5)), fill="blue")
     draw.text((30, 5),"Utilisez les flèches <- ->", font= myfont, fill=(255,0,0))
-    
+
     ##################################################################
     #
     #  gestion des FPS et de l'affiche écran | ne pas toucher
-    
+
     # transfert de la zone de dessin vers l'ecran
     photo = PIL.ImageTk.PhotoImage(buffer)
     canvas.itemconfig(ECRAN, image = photo)
-  
+
     # synchronisation a 20 fps
     timeend = time.time()
     delta = timeend-timestart - 1/20
@@ -144,12 +144,5 @@ while(OnBoucle):
        time.sleep(delta)
     #affichage
     canvas.update()
-    
-    
-    
-window.destroy()            
-            
 
-      
- 
-
+window.destroy()
