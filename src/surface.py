8baec8a993bda7d3fca215b1a9fbf2005b0e9d2d
Obsidienne <obsiagae@gmail.com>
2018-03-08 22:11:35 +0100

    Surface.rect + Surface.draw_*()

--- src/surface.py
+++ src/surface.py
@@ -4,5 +4,29 @@ class Surface:
     def __init__(self, pygsurface):
         self.pygsurface = pygsurface
 
-    def draw_surface(self, source, position, area = None):
-        self.pygsurface(source, position, area)
+    @property
+    def rect(self): return self.pygsurface.get_rect()
+
+    @property
+    def size(self): return self.rect.size
+
+    @property
+    def width(self): return self.rect.width
+
+    @property
+    def height(self): return self.rect.height
+
+    def fill(self, color):
+        self.pygsurface.fill(color)
+
+    def draw_surface(self, source, pos, area = None):
+        self.pygsurface.blit(source, pos, area)
+
+    def draw_rect(self, color, rect, width = 0):
+        pygame.draw.rect(self.pygsurface, color, rect, width)
+
+    def draw_circle(self, color, center, radius, width = 0):
+        pygame.draw.circle(self.pygsurface, color, center, radius, width)
+
+    def draw_line(self, color, start_pos, end_pos, width = 1):
+        pygame.draw.line(self.pygsurface, color, start_pos, end_pos, width)
