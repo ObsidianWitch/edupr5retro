fee13a7f9dcdee663e40b589d388225775af4ec6
Obsidienne <obsiagae@gmail.com>
2018-03-11 17:14:52 +0100

    constants - colors

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,15 @@
 import pygame
 from pygame.locals import *
 
-M_LEFT = 1
+M_LEFT   = 1
 M_MIDDLE = 2
-M_RIGHT = 3
+M_RIGHT  = 3
+
+BLACK   = (  0,   0,   0)
+WHITE   = (255, 255, 255)
+RED     = (255,   0,   0)
+GREEN   = (  0, 255,   0)
+BLUE    = (  0,   0, 255)
+CYAN    = (  0, 255, 255)
+MAGENTA = (255,   0, 255)
+YELLOW  = (255, 255,   0)
