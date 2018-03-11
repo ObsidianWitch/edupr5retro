c3b67016c4574ab565de82c51d948684f7184ec8
Obsidienne <obsiagae@gmail.com>
2018-03-11 19:56:22 +0100

    Font - use constants

--- src/font.py
+++ src/font.py
@@ -1,11 +1,12 @@
 import pygame
+from src.constants import *
 from src.image import Image
 class Font:
     def __init__(self, size):
         self.font = pygame.font.SysFont(None, size)
 
     # Crée une Image avec le texte spécifié dessus.
-    def render(self, text, antialias = False, color = (0, 0, 0), bgcolor = None):
+    def render(self, text, antialias = False, color = BLACK, bgcolor = None):
         return Image(
             self.font.render(text, antialias, color, bgcolor)
         )
