5808c8072bb74a080ef6ee7a48dfa6008e740a28
Obsidienne <obsiagae@gmail.com>
2018-03-12 23:14:35 +0100

    build.sh - update

--- build.sh
+++ build.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-decorate() {
-    echo "### begin $1" >> 'out/retro.py'
-    $2 "$1" >> 'out/retro.py'
-    echo "### end $1" >> 'out/retro.py'
+build() {
+    decorate() {
+        echo "### begin $1" >> 'out/retro.py'
+        $2 "$1" >> 'out/retro.py'
+        echo "### end $1" >> 'out/retro.py'
+    }
+
+    discard_imports() {
+        sed -e '/^import/d' \
+            -e '/^from .* import/d'
+    }
+
+    discard_comments() {
+        sed -e '/^\s*#/d'
+    }
+
+    discard_all() {
+        cat "$1" | discard_imports | discard_comments
+    }
+
+    mkdir -p 'out'
+    echo -n > 'out/retro.py'
+    decorate 'src/constants.py' 'cat'
+    for f in 'src/rect.py' \
+             'src/image.py' \
+             'src/font.py' \
+             'src/window.py' \
+             'src/event.py'
+    do decorate "$f" 'discard_all'; done
 }
 
-# Discard imports and comments.
-discard_elems() {
-    sed -e '/^import/d' \
-        -e '/^from .* import/d' \
-        -e '/^\s*#/d' \
-        "$1"
-}
-
-mkdir -p 'out'
-echo -n > 'out/retro.py'
-decorate 'src/constants.py' 'cat'
-for f in 'src/rect.py' \
-         'src/image.py' \
-         'src/font.py' \
-         'src/window.py' \
-         'src/event.py'
-do decorate "$f" 'discard_elems'; done
+build
