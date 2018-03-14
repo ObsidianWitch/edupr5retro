8355e17f2462ec0132ac16fb2f8d164b6fc5c399
Obsidienne <obsiagae@gmail.com>
2018-03-14 01:58:47 +0100

    build.sh - simplify

--- build.sh
+++ build.sh
@@ -1,44 +1,20 @@
 #!/bin/sh
 
-build() {
-    decorate() {
-        cat
-        echo
-    }
+dnewlines() { cat -s ; }
+dimports() { sed -e '/^import/d' -e '/^from .* import/d' ; }
+dcomments() { sed '/^\s*#/d' ; }
+decorate() { cat ; echo ; }
 
-    discard_newlines() {
-        cat -s
-    }
+mkdir -p 'out'
 
-    discard_imports() {
-        sed -e '/^import/d' \
-            -e '/^from .* import/d'
-    }
+out1='out/retro.full.py'
+cat 'src/constants.py' | dnewlines | decorate > "$out1"
+for f in 'src/rect.py' \
+         'src/image.py' \
+         'src/font.py' \
+         'src/window.py' \
+         'src/events.py'
+do cat "$f" | dimports | dnewlines | decorate >> "$out1"; done
 
-    discard_comments() {
-        sed '/^\s*#/d'
-    }
-
-    out1='out/retro.full.py'
-    mkdir -p `dirname "$out1"`
-    echo -n > "$out1"
-    cat 'src/constants.py' | discard_newlines | decorate >> "$out1"
-    for f in 'src/rect.py' \
-             'src/image.py' \
-             'src/font.py' \
-             'src/window.py' \
-             'src/events.py'
-    do cat "$f" | discard_imports | discard_newlines | decorate >> "$out1"; done
-
-    out2='out/retro.py'
-    echo -n > "$out2"
-    cat "$out1" | discard_comments | discard_newlines >> "$out2"
-}
-
-doc() {
-    out='doc/3_classes.md'
-    ./doc.py > "$out"
-}
-
-build
-doc
+out2='out/retro.py'
+cat "$out1" | dcomments | dnewlines > "$out2"
