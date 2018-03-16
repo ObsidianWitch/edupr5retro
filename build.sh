a8d92d8de827aa9ab8ec35c964e761ac2842754a
Obsidienne <obsiagae@gmail.com>
2018-03-16 04:40:00 +0100

    build.sh - retro.doc.py

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ dnewlines() { cat -s ; }
 dimports() { sed -e '/^import/d' -e '/^from .* import/d' ; }
 dcomments() { sed '/^\s*#/d' ; }
 decorate() { cat ; echo ; }
+fdoc() {
+    sed '/^\s*# /d' \
+  | sed '/^\s*## ~~~/d' \
+  | sed 's/##/#/' \
+  | sed '/> \[/d'
+}
 
 mkdir -p 'out'
 
@@ -16,5 +22,8 @@ for f in 'src/rect.py' \
          'src/events.py'
 do cat "$f" | dimports | dnewlines | decorate >> "$out1"; done
 
-out2='out/retro.py'
-cat "$out1" | dcomments | dnewlines > "$out2"
+out2='out/retro.doc.py'
+cat "$out1" | fdoc | dnewlines > "$out2"
+
+out3='out/retro.py'
+cat "$out1" | dcomments | dnewlines > "$out3"
