a1dd83040225b2f088e668f065b58e514b7e2b18
Obsidienne <obsiagae@gmail.com>
2018-03-19 20:50:43 +0100

    play_manual - simplify

--- play_manual.py
+++ play_manual.py
@@ -8,15 +8,14 @@ window = retro.Window(
 )
 events = retro.Events()
 
-game = Game(window, nbirds = 10)
+game = Game(window, nbirds = 1)
 
 while 1:
     events.update()
     if events.event(retro.QUIT): sys.exit()
 
     if not game.finished:
-        for i, b in enumerate(game.birds):
-            if b.alive and events.key_press(retro.K_SPACE): b.flap()
+        if events.key_press(retro.K_SPACE): game.birds[0].flap()
         game.run()
     else:
         game.reset()
