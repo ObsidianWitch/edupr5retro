c8726716c407dd0e3b79b6f98e5adc83048ba1ee
Obsidienne <obsiagae@gmail.com>
2018-03-25 08:17:44 +0200

    game - player + pixel collision (wrong approach)

--- game.py
+++ game.py
@@ -1,33 +1,82 @@
 import os
+import types
 import retro
 
 def assets(filename): return os.path.join("assets", filename)
 
-class Sprite:
-    def __init__(self, image):
-        self.image = image
-        self.rect = self.image.rect()
+class Collisions:
+    @classmethod
+    def pixel_checker(cls, image, color):
+        def inside(p): return image.rect().collidepoint(p)
 
-    def collide(self, *args):
-        for elem in args:
-            if self.rect.colliderect(elem): return True
-        return False
+        def check(p, offset):
+            p = (p[0] + offset[0], p[1] + offset[1])
+            if not inside(p): return None
+            return (image[p] == color)
 
-    def update(self): pass
-    def draw(self, image): image.draw_img(self.image, self.rect)
+        return check
+
+    @classmethod
+    def pixel_vertices(cls, image, rect, color):
+        check = cls.pixel_checker(image, color)
+
+        return types.SimpleNamespace(
+            up    = check(rect.topleft,     ( 0, -1))
+                 or check(rect.topright,    (-1, -1)),
+            down  = check(rect.bottomleft,  ( 0,  0))
+                 or check(rect.bottomright, (-1,  0)),
+            left  = check(rect.topleft,     (-1,  0))
+                 or check(rect.bottomleft,  (-1, -1)),
+            right = check(rect.topright,    ( 0,  0))
+                 or check(rect.bottomright, ( 0, -1)),
+        )
+
+class Maze(retro.Sprite):
+    IMG = retro.Image.from_path(assets("maze.png"))
+    C_WALL = (33, 33, 222)
+    def __init__(self, window):
+        retro.Sprite.__init__(self, self.IMG)
+
+class Player(retro.Sprite):
+    IMG = retro.Image.from_path(assets("pacman.png"))
+
+    def __init__(self):
+        retro.Sprite.__init__(self, self.IMG)
+        self.rect.move(208, 264)
+        self.speed = [0, 0]
+
+    def collide(self, maze):
+        collisions = Collisions.pixel_vertices(
+            image = maze.image,
+            rect  = self.rect,
+            color = Maze.C_WALL,
+        )
+
+        if   collisions.up    is None: self.rect.bottom = maze.rect.bottom
+        elif collisions.down  is None: self.rect.top    = maze.rect.top
+        elif collisions.left  is None: self.rect.right  = maze.rect.right
+        elif collisions.right is None: self.rect.left   = maze.rect.left
+
+        if collisions.up    and (self.speed[1] == -1): self.speed[1] = 0
+        if collisions.down  and (self.speed[1] ==  1): self.speed[1] = 0
+        if collisions.left  and (self.speed[0] == -1): self.speed[0] = 0
+        if collisions.right and (self.speed[0] ==  1): self.speed[0] = 0
+
+    def update(self):
+        self.rect.move(self.speed)
 
 class Game:
-    MAZE_IMG = retro.Image.from_path(assets("maze.png"))
-
     def __init__(self, window):
         self.window = window
-        self.maze = Sprite(self.MAZE_IMG.copy())
-        self.maze.rect.bottom = self.window.rect().bottom
+        self.maze = Maze(window)
+        self.player = Player()
 
     def run(self):
         # Update
-        # TODO
+        self.player.collide(self.maze)
+        self.player.update()
 
         # Draw
         self.window.fill(retro.BLACK)
         self.maze.draw(self.window)
+        self.player.draw(self.window)
--- play_manual.py
+++ play_manual.py
@@ -3,8 +3,9 @@ import retro
 from game import Game
 
 window = retro.Window(
-    title = "Pacman",
-    size  = (448, 528),
+    title     = "Pacman",
+    size      = (448, 528),
+    framerate = 60,
 )
 events = retro.Events()
 game = Game(window)
@@ -12,6 +13,11 @@ game = Game(window)
 while 1:
     events.update()
     if events.event(retro.QUIT): sys.exit()
+
+    elif events.key_press(retro.K_UP):    game.player.speed[1] = -1
+    if   events.key_press(retro.K_DOWN):  game.player.speed[1] =  1
+    elif events.key_press(retro.K_LEFT):  game.player.speed[0] = -1
+    elif events.key_press(retro.K_RIGHT): game.player.speed[0] =  1
 
     game.run()
 
