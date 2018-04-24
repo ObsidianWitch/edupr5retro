622063bbac62fa216fe3e7324e194d11ecedb414
Obsidienne <obsiagae@gmail.com>
2018-04-24 15:51:36 +0200

    manual - print maze and floor cells on key space

--- play_manual.py
+++ play_manual.py
@@ -15,13 +15,20 @@ while 1:
     if events.event(retro.QUIT): sys.exit()
 
     if not game.finished:
-        if   events.key_press(retro.K_UP):    game.player.nxtdir = [ 0, -1]
-        elif events.key_press(retro.K_DOWN):  game.player.nxtdir = [ 0,  1]
-        elif events.key_press(retro.K_LEFT):  game.player.nxtdir = [-1,  0]
-        elif events.key_press(retro.K_RIGHT): game.player.nxtdir = [ 1,  0]
-        elif events.key_press(retro.K_SPACE): game.maze.print(
-            game.player, game.ghosts
-        )
+        maze = game.maze
+        player = game.player
+        ghosts = game.ghosts
+
+        if   events.key_press(retro.K_UP):    player.nxtdir = [ 0, -1]
+        elif events.key_press(retro.K_DOWN):  player.nxtdir = [ 0,  1]
+        elif events.key_press(retro.K_LEFT):  player.nxtdir = [-1,  0]
+        elif events.key_press(retro.K_RIGHT): player.nxtdir = [ 1,  0]
+        elif events.key_press(retro.K_SPACE):
+            maze.print(player, ghosts)
+            print(maze.walls.floor_cells(
+                *maze.tile_pos(player.rect.center)
+            ))
+
         game.update()
         game.draw()
     else:
