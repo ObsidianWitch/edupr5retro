6b2259cf6e746fb9bd41bfd8ff725f4343f40121
Obsidienne <obsiagae@gmail.com>
2018-04-25 12:57:52 +0200

    nnga - update

--- play_nnga.py
+++ play_nnga.py
@@ -14,7 +14,7 @@ window = retro.Window(
 events = retro.Events()
 
 games = Games(window, size = 200)
-pool = NNGAPool(size = len(games), arch = (7, 10, 4))
+pool = NNGAPool(size = len(games), arch = (10, 10, 10, 4))
 
 def vsub(s1, s2): return (
     s1.rect.centerx - s2.rect.centerx,
@@ -34,10 +34,12 @@ while 1:
 
             p = pool[i].predict(
                 *vsub(ghost, player) if ghost else (-1.0, -1.0),
+                ghost.state.current if ghost else -1,
                 *vsub(bonus, player) if bonus else (-1.0, -1.0),
-                ghost.state.current if ghost else 0.0,
-                1.0 if player.curcol else 0.0,
-                1.0 if player.nxtcol else 0.0,
+                g.maze.bonuses.count,
+                *g.maze.walls.floor_cells(
+                    *g.maze.tile_pos(player.rect.center)
+                )
             )
 
             dirs = ([-1, 0], [ 0, -1], [ 1,  0], [ 0,  1])
