d447cc85ac6c86a852ae66e5576afcb8c64975c9
Obsidienne <obsidienne@protonmail.com>
2020-01-20 01:14:56 +0100

    maze - simplify fsm

--- maze/main.py
+++ maze/main.py
@@ -3,37 +3,31 @@ import shared.retro as retro
 from maze.states.run import StateRun
 from maze.states.end import StateEnd
 
+class Game:
+    def __init__(self, window):
+        self.window = window
+        self.state = None
+
+    def run(self):
+        if self.state is None:
+            self.state = StateRun(window)
+
+        elif type(self.state) == StateRun:
+            if self.state.win:
+                self.state = StateEnd(window)
+            else:
+                self.state.run()
+
+        elif type(self.state) == StateEnd:
+            if self.state.restart:
+                self.state = StateRun(window)
+            else:
+                self.state.run()
+
 window = retro.Window(
     title = "Maze",
     size  = (400, 400),
 )
 window.cursor(False)
-
-states = types.SimpleNamespace(
-    START    = 0,
-    RUN      = 1,
-    END      = 2,
-    current  = 0,
-    instance = None,
-)
-
-def game():
-    if states.current == states.START:
-        states.instance = StateRun(window)
-        states.current  = states.RUN
-
-    elif states.current == states.RUN:
-        if states.instance.win:
-            states.instance = StateEnd(window)
-            states.current  = states.END
-        else:
-            states.instance.run()
-
-    elif states.current == states.END:
-        if states.instance.restart:
-            states.instance = StateRun(window)
-            states.current  = states.RUN
-        else:
-            states.instance.run()
-
-window.loop(game)
+game = Game(window)
+window.loop(game.run)
