b44f0afbf991d72f76e9b48dc6442b715ec4214a
Obsidienne <obsidienne@protonmail.com>
2020-02-24 02:03:44 +0100

    retro - update

--- shared/retro.py
+++ shared/retro.py
@@ -26,6 +26,51 @@ class Math:
           else maxval if val > maxval \
           else val
 
+class Vec:
+    @classmethod
+    def neg(cls, va): return [
+        -a for a in va
+    ]
+
+    @classmethod
+    def add(cls, a, b): return [
+        c + d for c, d in cls.iterator(a, b)
+    ]
+
+    @classmethod
+    def sub(cls, a, b): return [
+        c - d for c, d in cls.iterator(a, b)
+    ]
+
+    @classmethod
+    def mul(cls, a, b): return [
+        c * d for c, d in cls.iterator(a, b)
+    ]
+
+    @classmethod
+    def dot(cls, a, b): return sum(
+        c * d for c, d in cls.iterator(a, b)
+    )
+
+    @classmethod
+    def eq(cls, a, b): return (len(a) == len(b)) and all(
+        c == d for c, d in cls.iterator(a, b)
+    )
+
+    @classmethod
+    def ne(cls, a, b): return (len(a) != len(b)) or all(
+        c != d for c, d in cls.iterator(a, b)
+    )
+
+    @classmethod
+    def iterator(cls, a, b):
+        if isinstance(a, Number):
+            for i, _ in enumerate(b): yield a, b[i]
+        elif isinstance(b, Number):
+            for i, _ in enumerate(a): yield a[i], b
+        else:
+            for i, _ in enumerate(a): yield a[i], b[i]
+
 class Rect(pygame.Rect):
 
     def __init__(self, *args):
@@ -385,49 +430,4 @@ class Stage(Sprite):
 
     def clear_focus(self):
         self.image.draw_img(self.original, self.camera.topleft, self.camera)
-
-class Vec:
-    @classmethod
-    def neg(cls, va): return [
-        -a for a in va
-    ]
-
-    @classmethod
-    def add(cls, a, b): return [
-        c + d for c, d in cls.iterator(a, b)
-    ]
-
-    @classmethod
-    def sub(cls, a, b): return [
-        c - d for c, d in cls.iterator(a, b)
-    ]
-
-    @classmethod
-    def mul(cls, a, b): return [
-        c * d for c, d in cls.iterator(a, b)
-    ]
-
-    @classmethod
-    def dot(cls, a, b): return sum(
-        c * d for c, d in cls.iterator(a, b)
-    )
-
-    @classmethod
-    def eq(cls, a, b): return (len(a) == len(b)) and all(
-        c == d for c, d in cls.iterator(a, b)
-    )
-
-    @classmethod
-    def ne(cls, a, b): return (len(a) != len(b)) or all(
-        c != d for c, d in cls.iterator(a, b)
-    )
-
-    @classmethod
-    def iterator(cls, a, b):
-        if isinstance(a, Number):
-            for i, _ in enumerate(b): yield a, b[i]
-        elif isinstance(b, Number):
-            for i, _ in enumerate(a): yield a[i], b
-        else:
-            for i, _ in enumerate(a): yield a[i], b[i]
 
