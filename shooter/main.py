82a829655a21ef53c400372d355b5eba75426e70
Obsidienne <obsidienne@protonmail.com>
2020-03-18 05:27:56 +0100

    shooter - merge states

--- shooter/main.py
+++ shooter/main.py
@@ -1,6 +1,5 @@
 import shared.retro as retro
-from shooter.states.run import StateRun
-from shooter.states.end import StateEnd
+from shooter import states
 
 class Game:
     def __init__(self, window):
@@ -9,17 +8,17 @@ class Game:
 
     def run(self):
         if self.state is None:
-            self.state = StateRun(window)
+            self.state = states.Run(window)
 
-        elif type(self.state) == StateRun:
+        elif type(self.state) == states.Run:
             if self.state.finished:
-                self.state = StateEnd(window)
+                self.state = states.End(window)
             else:
                 self.state.run()
 
-        elif type(self.state) == StateEnd:
+        elif type(self.state) == states.End:
             if self.state.restart:
-                self.state = StateRun(window)
+                self.state = states.Run(window)
             else:
                 self.state.run()
 
--- shooter/states.py
+++ shooter/states.py
@@ -3,7 +3,7 @@ from shooter.nodes.spawner import Spawner
 from shooter.nodes.player import Player
 from shooter.path import asset
 
-class StateRun:
+class Run:
     def __init__(self, window):
         self.window = window
 
@@ -40,3 +40,23 @@ class StateRun:
         self.player.ammunitions.draw(self.window)
         self.player.hints.draw(self.player, self.spawner.mob, self.window)
         self.spawner.mob.draw_shoot_timer(self.window.fonts[1], self.window)
+
+class End:
+    def __init__(self, window):
+        self.window = window
+        self.restart = False
+
+        self.txt = retro.Sprite(self.window.fonts[4].render(
+            text    = "DEAD",
+            color   = retro.WHITE,
+            bgcolor = retro.BLACK,
+        ))
+        self.txt.rect.center = self.window.rect().center
+
+    def run(self):
+        # Update
+        key = self.window.events.key_press
+        self.restart = key(retro.K_SPACE)
+
+        # Draw
+        self.txt.draw(self.window)
