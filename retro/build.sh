5718c85c6f148d77d8df7ee4e39027ed5e86528f
Obsidienne <obsidienne@protonmail.com>
2020-03-22 09:33:29 +0100

    retro.tests - headless sprite

--- retro/build.sh
+++ retro/build.sh
@@ -4,6 +4,7 @@ set -o errexit -o nounset
 
 python -m retro.tests.font
 python -m retro.tests.image
+python -m retro.tests.sprite
 
 mypy --namespace-packages --ignore-missing-imports \
      --disallow-untyped-calls --disallow-untyped-defs \
--- retro/tests/sprite.py
+++ retro/tests/sprite.py
@@ -1,44 +1,56 @@
-import os
+import sys
 from retro.src import retro
 from retro.tests.path import assets
 
+def TestSprite(target):
+    s1 = retro.Sprite.from_path(assets("img.png"))
+    s1.rect1 = s1.rect.copy()
+    s1.rect1.top = s1.rect.bottom + 2
+
+    s2 = retro.Sprite.from_spritesheet(
+        path = assets("spritesheet.png"),
+        animations = retro.Animations(
+            frame_size = (30, 30),
+            period = 100,
+            WALK_R = (tuple(reversed(range(0, 8))), 11),
+        ),
+    )
+    s2.rect.left = s1.rect.right
+    s2.rect1 = s2.rect.copy()
+    s2.rect1.top = s2.rect.bottom + 2
+
+    s3 = retro.Sprite.from_path(assets("trap.png"))
+    s3.rect.left = s2.rect.right
+    s3.dy = 1
+
+    def update():
+        if s3.rect.top < 0 or s3.rect.top > 50:
+            s3.dy *= -1
+        s3.rect.y += s3.dy
+
+        target.fill(retro.WHITE)
+        s1.draw(target)
+        s1.draw(target,
+            position = s1.rect1.topleft,
+            area = retro.Rect(10, 10, 30, 30))
+        s2.draw(target)
+        s2.draw(target,
+            position = s2.rect1.topleft,
+            area = retro.Rect(0, 10, 1000, 1000))
+        s3.draw(target)
+
+    return update
+
 window = retro.Window(
-    title = "window",
+    title = "test",
     size  = (320, 240),
-    fps   = 60,
+    headless = '--display' not in sys.argv,
 )
+test_sprite = TestSprite(window)
 
-s1 = retro.Sprite.from_path(assets("img.png"))
-s1.dy = 1
-
-s2 = retro.Sprite.from_spritesheet(
-    path = assets("spritesheet.png"),
-    animations = retro.Animations(
-        frame_size = (30, 30),
-        period = 100,
-        WALK_R = (tuple(reversed(range(0, 8))), 11),
-        WALK_L = (range(0, 8), 0),
-    ),
-)
-s2.dx = 1
-
-def main():
-    if s1.rect.top < 0:
-        s1.dy = 1
-    elif s1.rect.bottom >= window.rect().h:
-        s1.dy = -1
-    s1.rect.move_ip(0, s1.dy)
-
-    if s2.rect.left < 0:
-        s2.animations.set("WALK_R")
-        s2.dx = 1
-    elif s2.rect.right >= window.rect().w:
-        s2.animations.set("WALK_L")
-        s2.dx = -1
-    s2.rect.move_ip(s2.dx, 0)
-
-    window.fill(retro.WHITE)
-    s1.draw(window)
-    s2.draw(window, area = retro.Rect(15, 10, 100, 100))
-
-window.loop(main)
+if window.headless:
+    for _ in range(10): test_sprite()
+    # window.save(assets('expectation_sprite.png'))
+    assert window == retro.Image.from_path(assets('expectation_sprite.png'))
+else:
+    window.loop(test_sprite)
